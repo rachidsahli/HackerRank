5c56b4a2f66484d0386d33686c847e1b05526707
rachidsahli <rachidsahli144@gmail.com>
2025-02-02 22:35:21 +0100

    if-else.py

---
+++ Python/if-else.py
@@ -0,0 +1,15 @@
+try: 
+        n = int(input("Entrez un entier : "))
+
+        if n % 2 == 0 :
+            if 2 <= n <= 5 :
+                print("Not Weird")
+            elif 6 <= n <= 20 :
+                print("Weird")
+            elif n > 20 :
+                print("Not Weird")
+        else:
+            print("Weird")
+            
+except ValueError:
+     print("Veuillez entrer un nombre entier valide.")
